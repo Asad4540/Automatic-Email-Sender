db4ca22bf91521bfaa6500c275113a316634cf0d
Asad4540 <asadchaudhry02@gmail.com>
2024-03-08 01:46:47 +0530

    search bar and options added

--- src/Components/AddEmail.js
+++ src/Components/AddEmail.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Add,Search} from "@mui/icons-material";
+import React from "react";
+
+// Create a separate SearchBar component
+const SearchBar = ({ onSearch }) => {
+  return (
+    <div className="flex items-center border border-black rounded-sm my-5">
+      <input
+        type="text"
+        placeholder="Search Clients..."
+        className="outline-none p-2  flex-grow"
+        onChange={(e) => onSearch(e.target.value)}
+      />
+      <button className="ml-2">
+        <Search />
+      </button>
+    </div>
+  );
+};
 
 const AddEmail = () => {
   return (
-    <div>AddEmail</div>
+    <div>
+      <div className="bg-gray-100 p-4 py-8 shadow-lg w-full max-w-screen-lg mx-auto rounded-md" >
+        <div className="flex justify-start gap-6 text-xl ml-2">
+          <h1 className=" hover:font-semibold">East zone</h1>
+          <h1 className=" hover:font-semibold">Internal Stakeholders</h1>
+          <h1 className=" hover:font-semibold">West Zone</h1>
+        </div>
+        <div className="w-full max-w-screen-sm ml-2">
+          <SearchBar onSearch={(searchTerm) => console.log(searchTerm)} />
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
-export default AddEmail
+export default AddEmail;
